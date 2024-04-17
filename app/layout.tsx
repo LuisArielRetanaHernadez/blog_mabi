22d286d776f52dce8c8142d3e40476f24f05c54a
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-17 17:18:53 -0600

    agregando otro div dentro del container donde envolvemos el prop children

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <div className="container">
-          {children}
+          <div className="wrapper">{children}</div>
         </div>
       </body>
     </html>
