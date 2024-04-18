b6d4c9693cc5e891cdc3e5518c7a57c8c0850ef2
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-17 23:28:52 -0600

    importando los estilos a agregamos la clase container y agregamso un div con la clase ball entre la imagen sun y moon

--- components/themeToggle/ThemeToggle.tsx
+++ components/themeToggle/ThemeToggle.tsx
@@ -3,12 +3,13 @@ import Image from "next/image"
 import sun from "../../public/sun.png"
 import moon from "../../public/moon.png"
 
-import style from "./themeToggle.module.scss"
+import style from "./themeToggle.module.css"
 
 const ThemeToggle = () => {
   return (
     <div className={style.container}>
       <Image src={moon} alt="mon" width={14} height={14}/>
+      <div className={style.ball}></div>
       <Image src={sun} alt="sun" width={14} height={14}/>
     </div>
   )
