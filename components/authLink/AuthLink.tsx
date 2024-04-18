f77b432b6c5cb70fc57225ab8096aac102aea716
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-17 22:34:03 -0600

    creando una variable status para crear una validacion temporar si el usuario esta en autentificado o no

--- components/authLink/AuthLink.tsx
+++ components/authLink/AuthLink.tsx
@@ -1,9 +1,14 @@
+import Link from "next/link";
+
 const AuthLink = () => {
-  return (
-    <div>
-      <a href="/auth/google">Login with Google</a>
-    </div>
-  );
+  const status:string = "notauthenticated"
+  return <>
+    {status !== "authenticated" ? (
+      <Link href="/logout">Logout</Link>
+    ) : (
+      <Link href="/login">Login</Link>
+    )}
+  </>;
 }
 
 export default AuthLink;
