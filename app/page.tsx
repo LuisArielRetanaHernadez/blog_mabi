9782fab6c05e93cc9f5322cac85fc99dfb9d7306
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-17 19:57:04 -0600

    agregando/importando el componente Footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/footer/Footer";
 import NavBar from "@/components/navbar/NavBar";
 import Image from "next/image";
 import Link from "next/link";
@@ -6,6 +7,7 @@ export default function Home() {
   return (
     <div>
       <NavBar />
+      <Footer />
     </div>
   );
 }
