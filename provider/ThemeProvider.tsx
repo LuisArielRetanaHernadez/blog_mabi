b0b79b86c50c7dd7d5537ee7c809a44f5649b4d6
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-18 13:23:04 -0600

    agregando/importando el ThemeContext

--- provider/ThemeProvider.tsx
+++ provider/ThemeProvider.tsx
@@ -1,8 +1,11 @@
 "use client"
 
-import React from "react"
+import { ThemeContext } from "@/context/ThemeContext"
+import React, { useContext } from "react"
 
 const ThemeProvider = () => {
+  const theme = useContext(ThemeContext)
+
   return (
     <div>ThemeProvider</div>
   )
