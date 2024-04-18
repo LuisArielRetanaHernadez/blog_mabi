17e840cb0f4eb4f52fbedd1902be45a1b32a84df
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-18 17:30:53 -0600

    agregando las clases dle featured que son container, title, post, imgContainer, postTitle, description y button

--- components/featured/Featured.tsx
+++ components/featured/Featured.tsx
@@ -1,24 +1,25 @@
 import Image from "next/image"
 
+import style from "./featured.module.css"
+
 const Featured = () => {
   return (
-    <div>
-      <h1>
+    <div className={style.container}>
+      <h1 className={style.title}>
         <b>Hey, Lam dev here!</b> Discover my stories and creative ideas
       </h1>
-      <div>
-        <div>
-          <Image fill/>
+      <div className={style.post}>
+        <div className={style.imgContainer}>
         </div>
-        <div>
-         <h1>adipisicing elit. Pariatur saepe natus cumque amet totam possimus, hic ea neque assumenda, beatae in consequatur dicta dolorem</h1>
-          <p>
+        <div className={style.textContainer}>
+         <h1 className={style.postTitle}>adipisicing elit. Pariatur saepe natus cumque amet totam possimus, hic ea neque assumenda, beatae in consequatur dicta dolorem</h1>
+          <p className={style.description}>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit.
             Similique assumenda magnam nesciunt sequi recusandae minus 
             quo illum qui! Laudantium adipisci repudiandae assumenda!
             Magnam voluptate nesciunt neque nostrum odio ad fuga.
           </p>
-          <button>Read Mored</button>
+          <button className={style.button}>Read Mored</button>
         </div>
       </div>
     </div>
