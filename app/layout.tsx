1e3cf691badf210dac878bbf002c39b2b3fec009
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-17 20:07:21 -0600

    agregando/importando los componentes NavBar y Footer donde el NavBar va antes del prop children y el Footer va despues

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import NavBar from "@/components/navbar/NavBar";
+import Footer from "@/components/footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +20,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <div className="container">
+          <NavBar />
           <div className="wrapper">{children}</div>
+          <Footer />
         </div>
       </body>
     </html>
