da83e3e8693169a965bb2760e6a80437297d2d75
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-17 19:53:53 -0600

    agregando/importamos el componente NavBar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import NavBar from "@/components/navbar/NavBar";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
+      <NavBar />
     </div>
   );
 }
