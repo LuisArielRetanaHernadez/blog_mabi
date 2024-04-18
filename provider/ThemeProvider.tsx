8b273cd9f01fa5b6f2e6ee0e673cf9c719587209
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-18 14:20:23 -0600

    corregiando la obtencion del estado theme del ThemeContext

--- provider/ThemeProvider.tsx
+++ provider/ThemeProvider.tsx
@@ -4,7 +4,7 @@ import { ThemeContext } from "@/context/ThemeContext"
 import React, { useContext } from "react"
 
 const ThemeProvider = ({ children }: Readonly<{ children: React.ReactNode }>) => {
-  const theme = useContext(ThemeContext)
+  const { theme } = useContext(ThemeContext)
 
   return (
     <div className={theme}>{ children }</div>
