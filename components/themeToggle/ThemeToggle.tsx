72ff225036b5094094b7f025e70bf50e3c0e44ab
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-17 23:15:06 -0600

    agregando/importando las imagens sun y moon y las renderizamos en el componente Image. Tambien importamos los estilos del themeToggle y agregamos la clase container

--- components/themeToggle/ThemeToggle.tsx
+++ components/themeToggle/ThemeToggle.tsx
@@ -1,6 +1,16 @@
+import Image from "next/image"
+
+import sun from "../../public/sun.png"
+import moon from "../../public/moon.png"
+
+import style from "./themeToggle.module.scss"
+
 const ThemeToggle = () => {
   return (
-    <div>ThemeToggle</div>
+    <div className={style.container}>
+      <Image src={moon} alt="mon" width={14} height={14}/>
+      <Image src={sun} alt="sun" width={14} height={14}/>
+    </div>
   )
 }
 
