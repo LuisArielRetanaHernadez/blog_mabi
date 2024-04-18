78359505a36814c487d24178caf4f0fa98a9bd1b
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-17 20:08:58 -0600

    quitando y las importaciones de los componentes NavBar y Foooter

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
-import Footer from "@/components/footer/Footer";
-import NavBar from "@/components/navbar/NavBar";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      <NavBar />
-      <Footer />
     </div>
   );
 }
