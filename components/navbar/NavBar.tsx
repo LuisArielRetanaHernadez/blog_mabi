398b8a5a45ee7949229cb860f27fb27b1fe9d081
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-17 21:09:37 -0600

    agregando/importando el componente Image para usar las imagenes de la redes sociales (aun no proyectamos ninguna imagen) y agregamos los 3 links para la ruta Home, About y Contact

--- components/navbar/NavBar.tsx
+++ components/navbar/NavBar.tsx
@@ -1,9 +1,24 @@
 import React from "react"
 
+import style from "./navbar.module.css"
+import Image from "next/image"
+import Link from "next/link"
+
 const NavBar = () => {
   return (
-    <div>
-      NavBar
+    <div className={style.container}>
+      <div className={style.social}>
+        <Image src="..." alt="facebook" width={24} height={24}/>
+        <Image src="..." alt="instgam" width={24} height={24}/>
+        <Image src="..." alt="twitter" width={24} height={24}/>
+        <Image src="..." alt="tiktok" width={24} height={24}/>
+      </div>
+      <div className={style.logo}>Lamablog</div>
+      <div className={style.links}>
+        <Link href="/">Home</Link>
+        <Link href="/">About</Link>
+        <Link href="/">Contact</Link>
+      </div>
     </div>
   )
 }
