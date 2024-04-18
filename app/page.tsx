b443bbead7e312df489a3a9752d3bebf4c3c009f
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-17 19:52:47 -0600

    quitando el contenido del div principal

--- app/page.tsx
+++ app/page.tsx
@@ -2,25 +2,8 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
-  return <div>
-    <Link href={'/'}>
-      <h1>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit quas non fuga quam dolores repellendus pariatur omnis
-        nesciunt natus sapiente magni ratione amet, alias nihil culpa voluptas odio? Sapiente, quidem?
-        Aliquid, praesentium esse? Quas quis distinctio voluptatibus nesciunt dolores consequuntur itaque, et maxime quia!
-        Exercitationem laborum eius, nisi eum tempora nihil quod, illo facilis qui eligendi facere? Rerum, repudiandae assumenda?
-        Fugit eos deserunt quia ipsam consequuntur, animi earum, non suscipit blanditiis commodi natus ratione? Nostrum,
-      </h1>
-
-      itaque rem. Rerum, explicabo nostrum. Eaque non mollitia eligendi fuga, quas molestiae tempore maxime similique?
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati adipisci exercitationem dolor amet repellat 
-      sunt, commodi molestiae consectetur placeat sapiente quo quam laboriosam id officia consequatur nihil ipsam vel voluptas.
-      Adipisci aliquid cumque saepe quisquam tempore odit nobis, eius eaque id nisi aut tenetur qui explicabo at impedit 
-      minima non dolorem labore perspiciatis nostrum soluta natus tempora nulla sed. Praesentium.
-      Consequuntur voluptas deleniti maiores fugiat? Nobis, tempora harum odio sed omnis reprehenderit, et, 
-      in delectus rerum quo ratione veritatis porro nisi beatae? Sit laborum possimus molestiae ullam quisquam atque sunt?
-      Optio, repellat error? Quae accusantium quia placeat ipsam velit maxime ea illum illo? Explicabo tempore 
-      asperiores velit qui. At doloremque error doloribus ex eveniet velit illum adipisci quisquam quo reprehenderit.
-    </Link>
-  </div>;
+  return (
+    <div>
+    </div>
+  );
 }
