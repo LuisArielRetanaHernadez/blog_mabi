6f4f0353c38ace0e162af613bfd0cd5a1207c6bc
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-17 20:49:29 -0600

    agregando/importando los componentes Featured, CategoryList, CardList y Menu

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import style from "./globals.css";
+
+// components
+import Featured from "@/components/featured/Featured";
+import CategoryList from "@/components/categoryList/CategoryList";
+import CardList from "@/components/cardList/CardList";
+import Menu from "@/components/menu/Menu";
+
 export default function Home() {
   return (
-    <div>
+    <div className={style.container}>
+      <Featured />
+      <CategoryList />
+      <div className={style.content}>
+        <CardList />
+        <Menu />
+      </div>
     </div>
   );
 }
