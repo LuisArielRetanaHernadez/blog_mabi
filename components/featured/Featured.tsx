d1df7aaa73ff9bb3923aa5b413f0f087bfdcd527
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-18 17:10:12 -0600

    creando la estructura del componente Featured

--- components/featured/Featured.tsx
+++ components/featured/Featured.tsx
@@ -1,6 +1,27 @@
+import Image from "next/image"
+
 const Featured = () => {
   return (
-    <div>Featured</div>
+    <div>
+      <h1>
+        <b>Hey, Lam dev here!</b> Discover my stories and creative ideas
+      </h1>
+      <div>
+        <div>
+          <Image fill/>
+        </div>
+        <div>
+         <h1>adipisicing elit. Pariatur saepe natus cumque amet totam possimus, hic ea neque assumenda, beatae in consequatur dicta dolorem</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Similique assumenda magnam nesciunt sequi recusandae minus 
+            quo illum qui! Laudantium adipisci repudiandae assumenda!
+            Magnam voluptate nesciunt neque nostrum odio ad fuga.
+          </p>
+          <button>Read Mored</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
