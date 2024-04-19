f7a38ab019895f3a46bf316c374fb32f6ecd90c2
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-19 17:37:48 -0600

    agregando un post manual y creamos los estilos de las clases

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -2,6 +2,8 @@ import Menu from "@/components/menu/Menu"
 
 import Image from "next/image"
 
+import p1 from '../.././public/p1.jpeg'
+
 import styles from './singlePage.module.css'
 
 const SinglePage = () => {
@@ -9,10 +11,10 @@ const SinglePage = () => {
     <div className={styles.container}>
       <div className={styles.informationContainer}>
         <div className={styles.textContainer}>
-          <h1>perspiciatis eligendi repellendus accusamus facilis blanditiis</h1>
+          <h1 className={styles.title}>perspiciatis eligendi repellendus accusamus facilis blanditiis</h1>
           <div className={styles.user}>
             <div className={styles.userImageContainer}>
-              <Image className={styles.image} src='' alt="image p1" />
+              <Image className={styles.avatar} src={p1} alt="image p1" fill/>
             </div>
             <div className={styles.userTextContainer}>
               <span className={styles.username}>Jhon Doe</span>
@@ -21,13 +23,44 @@ const SinglePage = () => {
           </div>
         </div>
         <div className={styles.imageContainer}> 
-          <Image className={styles.image} src='' alt="imge p1" />
+          <Image className={styles.image} src={p1} alt="imge p1" fill />
         </div>
       </div>
       <div className={styles.content}>
         <div className={styles.post}>
-          <Menu />
+          <div className={styles.description}>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo quasi dolor 
+              ut non vel! Pariatur fugiat laudantium tempore, ea voluptate perferendis 
+              ad? Quos ut odit, aspernatur voluptas totam assumenda! Expedita!
+              Ullam ratione excepturi laboriosam voluptatibus optio labore architecto 
+              ducimus dolorum at a hic et perferendis vero, tempore doloremque quidem 
+              repellat quia consequuntur rerum repellendus nihil. Ipsam veniam 
+              culpa veritatis aut.
+            </p>
+            <h2>Lorem ipsum dolor sit amet consectetur</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo quasi dolor 
+              ut non vel! Pariatur fugiat laudantium tempore, ea voluptate perferendis 
+              ad? Quos ut odit, aspernatur voluptas totam assumenda! Expedita!
+              Ullam ratione excepturi laboriosam voluptatibus optio labore architecto 
+              ducimus dolorum at a hic et perferendis vero, tempore doloremque quidem 
+              repellat quia consequuntur rerum repellendus nihil. Ipsam veniam 
+              culpa veritatis aut.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo quasi dolor 
+              ut non vel! Pariatur fugiat laudantium tempore, ea voluptate perferendis 
+              ad? Quos ut odit, aspernatur voluptas totam assumenda! Expedita!
+              Ullam ratione excepturi laboriosam voluptatibus optio labore architecto 
+              ducimus dolorum at a hic et perferendis vero, tempore doloremque quidem 
+              repellat quia consequuntur rerum repellendus nihil. Ipsam veniam 
+              culpa veritatis aut.
+            </p>
+          </div>
         </div>
+        <Menu />
       </div>
     </div>
   )
