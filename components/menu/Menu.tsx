a6ee72286b8aad461c2ca3afd9d992e47d71b62e
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-18 20:39:38 -0600

    creando la estructura del menu

--- components/menu/Menu.tsx
+++ components/menu/Menu.tsx
@@ -1,7 +1,33 @@
+import Link from "next/link"
+
+import styles from "./menu.module.css"
+
+import Image from "next/image"
+
+import p1 from "../../public/p1.jpeg"
+
 const Menu = () => {
   return (
-    <div>
-      <h1>Menu</h1>
+    <div className={styles.container}>
+      <h2>{"Waht's hot"}</h2>
+      <h1>Most Popular</h1>
+      <div className={styles.items}>
+        <Link className={styles.item} href="/">
+          <div className={styles.imageContainer}>
+            <Image className={styles.image} src={p1} alt="iamge p1"/>
+          </div>
+          <div className={styles.textContainer}>
+            <span className={`${styles.category} ${styles.travel}`}>Travel</span>
+          </div>
+          <h3>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit
+          </h3>
+          <div className={styles.detail}>
+            <span className={styles.username}>Jhon Doe</span>
+            <span className={styles.date}>10.03.2023</span>
+          </div>
+        </Link>
+      </div>
     </div>
   )
 }
