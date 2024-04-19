4714ec6e0bad1882741c0db07d727bd8c55c3aa2
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-18 18:58:42 -0600

    creando la estructura del componente CategoryList

--- components/categoryList/CategoryList.tsx
+++ components/categoryList/CategoryList.tsx
@@ -1,6 +1,37 @@
+import Link from "next/link"
+
+import Image from "next/image"
+
+import style from "../../public/style.png"
+import fashion from "../../public/fashion.png"
+import food from "../../public/food.png"
+import travel from "../../public/travel.png"
+
+import styles from "./categoryList.module.css"
+
 const CategoryList = () => {
   return (
-    <div>CategoryList</div>
+    <div>
+      <h1>Popular Category</h1>
+      <div className={`${styles.category} ${styles.style}`}>
+        <Link href="/blog?cat=style">
+          <Image src={style} alt="imagen style" width={32} height={32} />
+          style
+        </Link>
+        <Link href="/blog?cat=style">
+          <Image src={fashion} alt="imagen style" width={32} height={32} />
+          fashin
+        </Link>
+        <Link href="/blog?cat=style">
+          <Image src={food} alt="imagen style" width={32} height={32} />
+          food
+        </Link>
+        <Link href="/blog?cat=style">
+          <Image src={travel} alt="imagen style" width={32} height={32} />
+          travel
+        </Link>
+      </div>
+    </div>
   )
 }
 
