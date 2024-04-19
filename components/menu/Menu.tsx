c91befea2c27b5295196ab5db62f3bd82d5489b9
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-19 15:30:05 -0600

    agregando/importando el componente MenuCategories

--- components/menu/Menu.tsx
+++ components/menu/Menu.tsx
@@ -3,6 +3,7 @@ import Link from "next/link"
 import styles from "./menu.module.css"
 
 import MenuPosts from "../menuPosts/MenuPosts"
+import MenuCategories from "../menuCategories/MenuCategories"
 
 const Menu = () => {
   return (
@@ -13,26 +14,8 @@ const Menu = () => {
 
       <h2 className={styles.subtitle}>Discovery by topic</h2>
       <h1 className={styles.title}>Categories</h1>
-      <div className={styles.categoryList}>
-        <Link className={`${styles.categoryItem} ${styles.style}`} href="/blog?cat=style">
-          style
-        </Link>
-        <Link className={`${styles.categoryItem} ${styles.travel}`} href="/blog?cat=travel">
-          Travel
-        </Link>
-        <Link className={`${styles.categoryItem} ${styles.food}`} href="/blog?cat=food">
-          food
-        </Link>
-        <Link className={`${styles.categoryItem} ${styles.fashion}`} href="/blog?cat=fashion">
-          fashion
-        </Link>
-        <Link className={`${styles.categoryItem} ${styles.culture}`} href="/blog?cat=culture">
-          culture
-        </Link>
-        <Link className={`${styles.categoryItem} ${styles.coding}`} href="/blog?cat=coding">
-          coding
-        </Link>
-      </div>
+
+      <MenuCategories />
 
       <h2 className={styles.subtitle}>Chosen</h2>
       <h1 className={styles.title}>Edit pick</h1>
