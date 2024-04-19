b4df04369e1024cf2425d052cc3b7b1c961b47fd
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-18 19:45:57 -0600

    creando la estructura del componente CardList

--- components/cardList/CardList.tsx
+++ components/cardList/CardList.tsx
@@ -1,8 +1,22 @@
+import Image from "next/image"
+
+import p1 from "../../public/p1.jpeg"
+
 import Pagination from "../pagination/Pagination"
+
+import styles from './cardList.module.css'
 
 const CardList = () => {
   return (
-    <div>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Recent Post</h1>
+      <div className={styles.posts}>
+        <div className={styles.imgContainer}>
+          <Image src={p1} alt="image p1" />
+        </div>
+        <div className={styles.textContainer}></div>
+      </div>
+
       <Pagination />
     </div>
   )
