246368f28de9a50cb7624fcb0e9601a17464c3ac
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-19 14:17:21 -0600

    creando la estructura  de los links de las diferentes categories

--- components/menu/Menu.tsx
+++ components/menu/Menu.tsx
@@ -71,6 +71,29 @@ const Menu = () => {
 
       </div>
 
+      <h2 className={styles.subtitle}>Discovery by topic</h2>
+      <h1 className={styles.title}>Categories</h1>
+      <div className={styles.categoryList}>
+        <Link className={`${styles.categoryItem} ${styles.style}`} href="/blog?cat=style">
+          style
+        </Link>
+        <Link className={`${styles.categoryItem} ${styles.travel}`} href="/blog?cat=travel">
+          Travel
+        </Link>
+        <Link className={`${styles.categoryItem} ${styles.food}`} href="/blog?cat=food">
+          food
+        </Link>
+        <Link className={`${styles.categoryItem} ${styles.fashion}`} href="/blog?cat=fashion">
+          fashion
+        </Link>
+        <Link className={`${styles.categoryItem} ${styles.culture}`} href="/blog?cat=culture">
+          culture
+        </Link>
+        <Link className={`${styles.categoryItem} ${styles.coding}`} href="/blog?cat=coding">
+          coding
+        </Link>
+      </div>
+
       <h2 className={styles.subtitle}>Chosen</h2>
       <h1 className={styles.title}>Edit pick</h1>
       <div className={styles.items}>
