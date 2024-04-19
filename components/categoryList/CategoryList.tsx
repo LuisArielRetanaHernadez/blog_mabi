e5878a3e32dba793f113cd92accb0b290562c0fa
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-18 19:16:18 -0600

    dando color a los links es tamano a las imagnes

--- components/categoryList/CategoryList.tsx
+++ components/categoryList/CategoryList.tsx
@@ -15,19 +15,19 @@ const CategoryList = () => {
       <h1 className={styles.title}>Popular Category</h1>
       <div className={styles.categories}>
         <Link className={`${styles.category} ${styles.style}`} href="/blog?cat=style">
-          <Image src={style} alt="imagen style" width={32} height={32} />
+          <Image className={styles.image} src={style} alt="imagen style" width={32} height={32} />
           style
         </Link>
-        <Link className={`${styles.category} ${styles.style}`} href="/blog?cat=style">
-          <Image src={fashion} alt="imagen style" width={32} height={32} />
+        <Link className={`${styles.category} ${styles.fashion}`} href="/blog?cat=style">
+          <Image className={styles.image} src={fashion} alt="imagen style" width={32} height={32} />
           fashin
         </Link>
-        <Link className={`${styles.category} ${styles.style}`} href="/blog?cat=style">
-          <Image src={food} alt="imagen style" width={32} height={32} />
+        <Link className={`${styles.category} ${styles.food}`} href="/blog?cat=style">
+          <Image className={styles.image} src={food} alt="imagen style" width={32} height={32} />
           food
         </Link>
-        <Link className={`${styles.category} ${styles.style}`} href="/blog?cat=style">
-          <Image src={travel} alt="imagen style" width={32} height={32} />
+        <Link className={`${styles.category} ${styles.travel}`} href="/blog?cat=style">
+          <Image className={styles.image} src={travel} alt="imagen style" width={32} height={32} />
           travel
         </Link>
       </div>
