b4f230cab683dd62fbd2f4539a4217722ffe1691
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-19 14:07:18 -0600

    agregando otra seccion de categorys pero sin las imagenes

--- components/menu/Menu.tsx
+++ components/menu/Menu.tsx
@@ -18,6 +18,64 @@ const Menu = () => {
       <div className={styles.items}>
 
         <Link className={styles.item} href="/">
+          <div className={styles.textContainer}>
+            <span className={`${styles.category} ${styles.travel}`}>Travel</span>
+            <h3 className={styles.postTitle}>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit
+            </h3>
+            <div className={styles.detail}>
+              <span className={styles.username}>Jhon Doe</span>
+              <span className={styles.date}>10.03.2023</span>
+            </div>
+          </div>
+        </Link>
+
+        <Link className={styles.item} href="/">
+          <div className={styles.textContainer}>
+            <span className={`${styles.category} ${styles.fashion}`}>fashion</span>
+            <h3 className={styles.postTitle}>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit
+            </h3>
+            <div className={styles.detail}>
+              <span className={styles.username}>Jhon Doe</span>
+              <span className={styles.date}>10.03.2023</span>
+            </div>
+          </div>
+        </Link>
+
+        <Link className={styles.item} href="/">
+          <div className={styles.textContainer}>
+            <span className={`${styles.category} ${styles.culture}`}>culture</span>
+            <h3 className={styles.postTitle}>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit
+            </h3>
+            <div className={styles.detail}>
+              <span className={styles.username}>Jhon Doe</span>
+              <span className={styles.date}>10.03.2023</span>
+            </div>
+          </div>
+        </Link>
+
+        <Link className={styles.item} href="/">
+          <div className={styles.textContainer}>
+            <span className={`${styles.category} ${styles.food}`}>food</span>
+            <h3 className={styles.postTitle}>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit
+            </h3>
+            <div className={styles.detail}>
+              <span className={styles.username}>Jhon Doe</span>
+              <span className={styles.date}>10.03.2023</span>
+            </div>
+          </div>
+        </Link>
+
+      </div>
+
+      <h2 className={styles.subtitle}>Chosen</h2>
+      <h1 className={styles.title}>Edit pick</h1>
+      <div className={styles.items}>
+
+        <Link className={styles.item} href="/">
           <div className={styles.imageContainer}>
             <Image className={styles.image} src={p1} alt="iamge p1" fill/>
           </div>
