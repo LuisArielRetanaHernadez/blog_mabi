0338005968fa306a12dd173fac148a197fdbb334
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-20 18:33:21 -0600

    agregando/importando singIn y useSession, usamos la funcion para autentificar googel. Creamos los estados del hooks de useSession

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,11 +1,19 @@
+"use client"
+import { signIn, useSession } from "next-auth/react"
+
 import styles from "./login.module.css"
 
 
 const Login = () => {
+  const [data, status] = useSession()
+
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
-        <div className={styles.socialButton}>Sing in with Google</div>
+        <div 
+        className={styles.socialButton}
+        onClick={() => signIn("google")}
+        >Sing in with Google</div>
         <div className={styles.socialButton}>Sing in with Github</div>
         <div className={styles.socialButton}>Sing in with Facebook</div>
       </div>
