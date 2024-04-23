95c8e3f44cec204dc84925658911ddf989965028
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-22 21:23:21 -0600

    creando los props key y pros y las tipasmos con el interfaz Props

--- components/card/Card.tsx
+++ components/card/Card.tsx
@@ -5,24 +5,27 @@ import p1 from "../../public/p1.jpeg"
 import styles from "./card.module.css"
 import Link from "next/link"
 
-const Card = () => {
+interface Props {
+  post: any
+  key: number
+}
+
+const Card = ({key, post}: Props) => {
   return (
-    <div className={styles.container}>
+    <div className={styles.container} key={key}>
       <div className={styles.imageContainer}>
-        <Image className={styles.image} src={p1} alt="image p1" fill />
+        <Image className={styles.image} src={post.image} alt="image p1" fill />
       </div>
       <div className={styles.textContainer}>
         <div className={styles.datail}>
-          <span className={styles.date}>11.02.2023 - </span>
-          <span className={styles.category}>CULTURE</span>
+          <span className={styles.date}>{post.date}</span>
+          <span className={styles.category}>{post.title}</span>
         </div>
         <Link href="/">
           <h1>Ad, veritatis accusamus similique hic sapiente tempore? Veniam itaque magnam delectus reiciendis quo</h1>
         </Link> 
         <p className={styles.description}>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, sapiente similique 
-          accusamus illo repellat quis aliquam fugiat vel, debitis cupiditate molestiae perspiciatis 
-          corrupti? Error aperiam, commodi nisi recusandae autem illo.
+          {post.description}
         </p>
         <Link className={styles.link} href="/">Read More</Link>
       </div>
