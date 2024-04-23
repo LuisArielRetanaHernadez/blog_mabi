a0b3b80a49e3ec45a84c354d5f6778b3de76598b
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-23 15:22:47 -0600

    creando la prop searchParams y accedemos al valor cat y page para despues se la pasamos a al componente CardList

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -3,12 +3,16 @@ import Menu from "@/components/menu/Menu"
 
 import styles from "./pageBlog.module.css"
 
-const Blog = () => {
+const Blog = ({ searchParams }: any) => {
+
+  const page = searchParams.page || 1
+  const { cat } = searchParams
+
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Style Blog</h1>
+      <h1 className={styles.title}>{cat}</h1>
       <div className={styles.content}>
-        <CardList />
+        <CardList page={page} cat={cat} />
         <Menu />
       </div>
     </div>
