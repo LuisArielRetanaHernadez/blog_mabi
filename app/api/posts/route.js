1492be02cd59cfb20eac88e788ee21d024d05a53
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-22 20:41:44 -0600

    obteniendo la parametro searchParams de la url del enpoint para limitar la llamada de los recuersos

--- app/api/posts/route.js
+++ app/api/posts/route.js
@@ -3,9 +3,19 @@ import { NextResponse } from "next/server";
 import prisma from "@/utils/connect";
 
 export const GET = async (request) => {
+  const { searchParams } = new URL(request.url)
+
+  const page = searchParams.get('page')
+
+  const take = 2
+  const skip = (page - 1) * take
+
   try {
-    const posts = await prisma.posts.findMany()
-    return NextResponse.json(posts)
+    const posts = await prisma.posts.findMany({
+      take,
+      skip,
+    })
+    return NextResponse.json(posts, {status: 2002})
   } catch (error) {
     return NextResponse.json(JSON.stringify({message: 'error'}))
   }
