a780c7928386f61bbb4f5897c986654463e57821
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-22 18:03:10 -0600

    corregiendo la conetacion de caregories a category

--- app/api/category/route.js
+++ app/api/category/route.js
@@ -4,7 +4,7 @@ import prisma from "@/utils/connect";
 
 export const GET = async (request) => {
   try {
-    const categories = await prisma.categories.findMany()
+    const categories = await prisma.category.findMany()
     return NextResponse.json(JSON.stringify(categories))
   } catch (error) {
     return NextResponse.json(JSON.stringify({message: 'error'}))
