2ff3b34cae0a2b68dc54a6829849dd14727829ec
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-23 15:39:20 -0600

    agregando el query para obtener los posts por sky, take pero tambien por categoria

--- app/api/posts/route.js
+++ app/api/posts/route.js
@@ -6,15 +6,22 @@ export const GET = async (request) => {
   const { searchParams } = new URL(request.url)
 
   const page = searchParams.get('page')
+  const cat = searchParams.get('cat')
 
   const take = 2
   const skip = (page - 1) * take
 
   try {
-    const posts = await prisma.posts.findMany({
-      take,
-      skip,
-    })
+    const posts = await prisma.posts.$transaction([
+      prisma.posts.findMany({
+        skip,
+        take,
+        where: {
+          ...(cat ? {category: cat} : null)
+        }
+      }),
+      prisma.posts.count(),
+    ])
     return NextResponse.json(posts, {status: 2002})
   } catch (error) {
     return NextResponse.json(JSON.stringify({message: 'error'}))
