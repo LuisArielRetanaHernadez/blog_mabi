6f60e8a0fcd69addc786bac8ca5598786a0eca52
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-22 19:18:05 -0600

    creando la funcion getData para obtener los posts y creamos la interfaz para tipar la devolucion de la funcion

--- components/cardList/CardList.tsx
+++ components/cardList/CardList.tsx
@@ -3,6 +3,33 @@ import Pagination from "../pagination/Pagination"
 import styles from './cardList.module.css'
 import Card from "../card/Card"
 
+interface Post {
+  id: number
+  slug: string
+  title: string
+  description: string
+  image: string
+  views: number
+  catSlug: string
+  cat: any 
+  userEmail: any 
+  user: any
+  comments: any 
+  createAt: Date 
+}
+
+const getData = async (): Promise<Post> => {
+  const res = await fetch("http://localhost:3000/aapi/posts", {
+    cache: "no-store"
+  })
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data")
+  }
+
+  return res.json()
+}
+
 const CardList = () => {
   return (
     <div className={styles.container}>
