ddd58a625da3bf1b4bf010c210b5347d2dd6113a
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-23 16:07:57 -0600

    mostrando  los datos del post

--- components/card/Card.tsx
+++ components/card/Card.tsx
@@ -13,21 +13,24 @@ interface Props {
 const Card = ({key, post}: Props) => {
   return (
     <div className={styles.container} key={key}>
-      <div className={styles.imageContainer}>
-        <Image className={styles.image} src={post.image} alt="image p1" fill />
-      </div>
+      {post.image && (
+        <div className={styles.imageContainer}>
+          <Image className={styles.image} src={post.image} alt="image p1" fill />
+        </div>
+      )}
+
       <div className={styles.textContainer}>
         <div className={styles.datail}>
           <span className={styles.date}>{post.date.substring(0, 10)}</span>
           <span className={styles.category}>{post.catSlug}</span>
         </div>
-        <Link href="/">
-          <h1>Ad, veritatis accusamus similique hic sapiente tempore? Veniam itaque magnam delectus reiciendis quo</h1>
+        <Link href={`/posts/${post.slug}`}>
+          <h1>{post.title}</h1>
         </Link> 
         <p className={styles.description}>
-          {post.description}
+          {post.description.substring(0, 10)}
         </p>
-        <Link className={styles.link} href="/">Read More</Link>
+        <Link className={styles.link} href={`/posts/${post.slug}`}>Read More</Link>
       </div>
     </div>
   )
