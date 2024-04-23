ad3f44c639ed465a10ae23e991afce7416a6a514
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-22 18:47:04 -0600

    quitando el JSON.stringfy en el response

--- app/api/category/route.js
+++ app/api/category/route.js
@@ -5,7 +5,7 @@ import prisma from "@/utils/connect";
 export const GET = async (request) => {
   try {
     const categories = await prisma.category.findMany()
-    return NextResponse.json(JSON.stringify(categories))
+    return NextResponse.json(categories)
   } catch (error) {
     return NextResponse.json(JSON.stringify({message: 'error'}))
   }
