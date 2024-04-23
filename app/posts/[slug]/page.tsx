3b35f70eb8e7b524b42a580015f7a41545d5b9dd
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-23 17:10:32 -0600

    creando la funcion getData para obtener el post de la api/posts/[slug] y mostramos su datos

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -7,59 +7,60 @@ import p1 from '../.././public/p1.jpeg'
 import styles from './singlePage.module.css'
 import Comments from "@/components/comments/Comments"
 
-const SinglePage = () => {
+interface Post {
+  id: number
+  slug: string
+  title: string
+  description: string
+  image: string
+  views: number
+  catSlug: string
+  cat: any 
+  userEmail: any 
+  user: any
+  comments: any 
+  createAt: Date 
+}
+
+const getData = async (slug: string): Promise<Post> => {
+  const res = await fetch(`http://localhost:3000/api/posts/${slug}`, {
+    cache: 'no-store'
+  }).then(res => res.json())
+
+  return res
+}
+
+const SinglePage = async ({ params }: any) => {
+  const { slug } = params;
+
+  const data = await getData(slug)
+
   return (
     <div className={styles.container}>
       <div className={styles.informationContainer}>
         <div className={styles.textContainer}>
-          <h1 className={styles.title}>perspiciatis eligendi repellendus accusamus facilis blanditiis</h1>
+          <h1 className={styles.title}>{data.title}</h1>
           <div className={styles.user}>
-            <div className={styles.userImageContainer}>
-              <Image className={styles.avatar} src={p1} alt="image p1" fill/>
-            </div>
+            {data.image && (
+              <div className={styles.userImageContainer}>
+                <Image className={styles.avatar} src={data.image} alt="image p1" fill/>
+              </div>
+            )}
             <div className={styles.userTextContainer}>
               <span className={styles.username}>Jhon Doe</span>
               <span className={styles.date}>01.01.2023</span>
             </div>
           </div>
         </div>
-        <div className={styles.imageContainer}> 
-          <Image className={styles.image} src={p1} alt="imge p1" fill />
-        </div>
+        { data.image && (
+          <div className={styles.imageContainer}> 
+            <Image className={styles.image} src={p1} alt="imge p1" fill />
+          </div>
+        )}
       </div>
       <div className={styles.content}>
         <div className={styles.post}>
-          <div className={styles.description}>
-
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo quasi dolor 
-              ut non vel! Pariatur fugiat laudantium tempore, ea voluptate perferendis 
-              ad? Quos ut odit, aspernatur voluptas totam assumenda! Expedita!
-              Ullam ratione excepturi laboriosam voluptatibus optio labore architecto 
-              ducimus dolorum at a hic et perferendis vero, tempore doloremque quidem 
-              repellat quia consequuntur rerum repellendus nihil. Ipsam veniam 
-              culpa veritatis aut.
-            </p>
-            <h2>Lorem ipsum dolor sit amet consectetur</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo quasi dolor 
-              ut non vel! Pariatur fugiat laudantium tempore, ea voluptate perferendis 
-              ad? Quos ut odit, aspernatur voluptas totam assumenda! Expedita!
-              Ullam ratione excepturi laboriosam voluptatibus optio labore architecto 
-              ducimus dolorum at a hic et perferendis vero, tempore doloremque quidem 
-              repellat quia consequuntur rerum repellendus nihil. Ipsam veniam 
-              culpa veritatis aut.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo quasi dolor 
-              ut non vel! Pariatur fugiat laudantium tempore, ea voluptate perferendis 
-              ad? Quos ut odit, aspernatur voluptas totam assumenda! Expedita!
-              Ullam ratione excepturi laboriosam voluptatibus optio labore architecto 
-              ducimus dolorum at a hic et perferendis vero, tempore doloremque quidem 
-              repellat quia consequuntur rerum repellendus nihil. Ipsam veniam 
-              culpa veritatis aut.
-            </p>
-          </div>
+          <div className={styles.description} dangerouslySetInnerHTML={{ __html: data?.description }} />
           <div className={styles.comment}>
             <Comments />
           </div>
