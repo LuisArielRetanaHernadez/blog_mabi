e0cbfdc5ee7f42a2e720d6aa75329aa8b0ad666a
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-23 18:50:24 -0600

    creando la funcion POST en al api/comments para crear un nuevo comentario

--- app/api/comments/route.js
+++ app/api/comments/route.js
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 
 import prisma from "@/utils/connect";
+import { getAuthSession } from "../auth/[...nextauth]/route";
 
 export const GET = async (request) => {
   const { searchParams } = new URL(request.url);
@@ -16,6 +17,27 @@ export const GET = async (request) => {
       }
     })
     return NextResponse.json(comments, {status: 2002})
+  } catch (error) {
+    return NextResponse.json(JSON.stringify({message: 'error'}))
+  }
+}
+
+export const POST = async (request) => {
+  const session = getAuthSession()
+
+  if (!session) {
+    return NextResponse.json(JSON.stringify({message: 'Not Authenti'}, {status: 401}))
+  }
+
+  try {
+    const body = await request.json()
+    const comment = await prisma.comments.create({
+      data: {
+        ...body,
+        userEmail: session.user.email
+      }
+    })
+    return NextResponse.json(comment, {status: 2002})
   } catch (error) {
     return NextResponse.json(JSON.stringify({message: 'error'}))
   }
