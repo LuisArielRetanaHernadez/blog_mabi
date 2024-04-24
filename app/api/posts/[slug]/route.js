bc04f5121e0cbe8d0e3d0b4165b4e434bb44f52b
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-23 21:46:37 -0600

    actualizando el campo viewr en la api posts/[slug] cada vez que un usuario entre a ver la post

--- app/api/posts/[slug]/route.js
+++ app/api/posts/[slug]/route.js
@@ -7,9 +7,14 @@ export const GET = async (request, { params }) => {
 
   try {
 
-    const post = await prisma.post.findUnique({
+    const post = await prisma.post.update({
       where: {
         slug
+      },
+      data: {
+        views: {
+          increment: 1
+        }
       },
       inclue: { 
         user: true
