7270ed9ab40ecd4b9183c772cb4a953c6de33769
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-23 19:26:39 -0600

    creando el archivo firabase en la carpeta utils donde copiamos el codigo de firabase configuracion

---
+++ utils/firabase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyADkw5Dy9k8uEnzeHm5Vi9c7_QZ4ebm2KE",
+  authDomain: "blog-aab7a.firebaseapp.com",
+  projectId: "blog-aab7a",
+  storageBucket: "blog-aab7a.appspot.com",
+  messagingSenderId: "681280121329",
+  appId: "1:681280121329:web:8a9d95b2396c0b0b847c50"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
