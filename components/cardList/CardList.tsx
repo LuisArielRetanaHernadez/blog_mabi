aa95ee30bafe900f36f28f8abe33c63fa9ddf832
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-24 20:01:02 -0600

    creando la interfaz Data para tipar la funcion getData y cambiamos la forma de acceder a los datos devueltos por la funcion getData

--- components/cardList/CardList.tsx
+++ components/cardList/CardList.tsx
@@ -18,7 +18,12 @@ interface Post {
   createAt: Date 
 }
 
-const getData = async (page: number, cat: string): Promise<Post[]> => {
+interface Data {
+  posts: Post[]
+  count: number
+}
+
+const getData = async (page: number, cat: string): Promise<Data> => {
   const res = await fetch(`http://localhost:3000/api/posts?page=${page}&cat=${cat || ""}`, {
     cache: "no-store"
   }).then(res => res.json())
@@ -32,15 +37,16 @@ const CardList = async ({ page, cat }: {page: number, cat?: string}) => {
   const postsShow = 2
 
   const hasPrev = postsShow * (page - 1) > 0
-  const hasNext = postsShow * page + postsShow < data.length
+  const hasNext = postsShow * page + postsShow < data.posts.length
 
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Recent Post</h1>
       <div className={styles.posts}>
-        {data?.length ?? data?.map(post => (
+        {data?.count ? data?.posts?.map(post => (
           <Card key={post.id} post={post}/>
-        )
+        )) : (
+          <h3>No posts</h3>
         )}
       </div>
       <Pagination page={page} hasPrev={hasPrev} hasNext={hasNext}/>
