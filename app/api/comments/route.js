6bb228e0682988ce1ec85dac05e77387380772fb
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-26 14:10:00 -0600

    agregando el await en la funcion getAuthSession  y creamos el objeto dataPosts para crear la estructura de los datos para crear un nuevo comentario

--- app/api/comments/route.js
+++ app/api/comments/route.js
@@ -5,10 +5,10 @@ import { getAuthSession } from "../auth/[...nextauth]/route";
 
 export const GET = async (request) => {
   const { searchParams } = new URL(request.url);
-  const postSlug = searchParams.get("slug");
+  const postSlug = searchParams.get("postSlug");
 
   try {
-    const comments = await prisma.comments.findMany({
+    const comments = await prisma.comment.findMany({
       where: {
         ...(postSlug && { postSlug })
       },
@@ -16,29 +16,32 @@ export const GET = async (request) => {
         user: true
       }
     })
-    return NextResponse.json(comments, {status: 2002})
+    return NextResponse.json(comments, {status: 202})
   } catch (error) {
     return NextResponse.json(JSON.stringify({message: 'error'}))
   }
 }
 
 export const POST = async (request) => {
-  const session = getAuthSession()
+  const session = await getAuthSession()
 
   if (!session) {
-    return NextResponse.json(JSON.stringify({message: 'Not Authenti'}, {status: 401}))
+    return NextResponse.json({message: 'Not Authenti'}, {status: 401})
   }
 
   try {
     const body = await request.json()
-    const comment = await prisma.comments.create({
-      data: {
-        ...body,
-        userEmail: session.user.email
-      }
+
+    const dataPost = {
+      ...body,
+      userEmail: session.user.email
+    }
+
+    const comment = await prisma.comment.create({
+      data: dataPost
     })
-    return NextResponse.json(comment, {status: 2002})
+    return NextResponse.json(comment, {status: 202})
   } catch (error) {
-    return NextResponse.json(JSON.stringify({message: 'error'}))
+    return NextResponse.json({message: 'error'})
   }
 }
