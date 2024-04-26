152f72869c57627ace24061d12ceb15d70dae967
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-04-26 14:07:08 -0600

    corregiendo el nombre de la props del componenete Comments

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -62,7 +62,7 @@ const SinglePage = async ({ params }: any) => {
         <div className={styles.post}>
           <div className={styles.description} dangerouslySetInnerHTML={{ __html: data?.description }} />
           <div className={styles.comment}>
-            <Comments slug={slug}/>
+            <Comments postSlug={slug}/>
           </div>
         </div>
         <Menu />
